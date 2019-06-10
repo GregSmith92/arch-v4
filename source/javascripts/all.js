11377b1be88314e6dcf7d80bb6bfea2f07722722
Greg Smith <greg_smith1992@hotmail.com>
2019-06-10 19:00:13 +0100

    Added jquery to add opacity to title on scroll down

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,7 +4,7 @@
 
 const backBox = document.getElementById("back-box2");
 const smallBox = document.getElementById("back-box2-small");
-// console.log(smallBox);
+
 
 const rotateBig = (query) => {
   window.addEventListener("scroll", (event) => {
@@ -17,6 +17,12 @@ const rotateSmall = (query) => {
     smallBox.style.transform = "rotate(-"+(window.pageYOffset / 2)+"deg)";
   });
 };
+
+
+$(window).scroll(function(){
+    $(".header").css("opacity", 1 - $(window).scrollTop() / 600);
+  });
+
 
 rotateBig();
 rotateSmall();
