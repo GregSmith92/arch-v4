b5a491d76508db5e3896df31227e72b0698554b0
Greg Smith <greg_smith1992@hotmail.com>
2019-06-10 12:31:56 +0100

    Added JS for rotation of elements

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,16 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+const backBox = document.getElementById("back-box");
+const headers = document.querySelector(".header");
+console.log(headers);
+console.log(backBox);
+
+const rotate = (query) => {
+  window.addEventListener("scroll", (event) => {
+    backBox.style.transform = "rotate("+window.pageYOffset+"deg)";
+  });
+};
+
+rotate();
